4c35e523648c192a6958b0dc22d95b6ede22af27
947851266@qq.com <947851266@qq.com>
2017-12-09 22:19:29 +0800

    12

--- src/com/modifyIP/IPModify.java
+++ src/com/modifyIP/IPModify.java
@@ -9,7 +9,6 @@ public class IPModify {
 //		Runtime.getRuntime().exec("netsh    interface    ip    set    addr    \"本地连接\"    static    "
 //							+ newip + "    255.255.255.0     10.0.0.1     1");
 		String comm = "\"cmd\" \"/c\" \"mypro\" \"D:\\mysql\\mysql-5.6.25-winx64\\bin\"";
-
 		Runtime.getRuntime().exec(comm);
 		System.out.println("sdfdsf");
 	}
